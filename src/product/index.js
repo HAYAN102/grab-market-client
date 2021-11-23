117a6d0adc745d9e95c28e5f450b2c3fb7888114
HAYAN102 <91953631+HAYAN102@users.noreply.github.com>
2021-11-23 20:26:24 +0900

    1123_슬라이딩배너

--- src/product/index.js
+++ src/product/index.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import "./index.css";
 import { API_URL } from "../config/constants.js";
+import dayjs from "dayjs";
 
 function ProductPage() {
   const { id } = useParams();
@@ -26,7 +27,7 @@ function ProductPage() {
   return (
     <div>
       <div id="image-box">
-        <img src={"/" + product.imageUrl} />
+        <img src={`${API_URL}/${product.imageUrl}`} />
       </div>
       <div id="profile-box">
         <img src="/images/icons/avatar.png" />
@@ -35,8 +36,10 @@ function ProductPage() {
       <div id="contents-box">
         <div id="name">{product.name}</div>
         <div id="price">{product.price}원</div>
-        <div id="createdAt">2021년 10월 12일</div>
-        <div id="description">{product.description}</div>
+        <div id="createdAt">
+          {dayjs(product.createdAt).format("YYYY년 MM월 DD일")}
+        </div>
+        <pre id="description">{product.description}</pre>
       </div>
     </div>
   );
